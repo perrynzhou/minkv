6bbff085fb12cf4baa9b045ccfdb785cc72776b5
perrynzhou <perrynzhou@gmail.com>
2020-08-17 18:04:25 +0800

    add benchmark used lua to impl

--- object.h
+++ object.h
@@ -7,5 +7,15 @@
 
 #ifndef _OBJECT_H
 #define _OBJECT_H
-
+#include <stdint.h>
+typedef enum object_type_t {
+  OBJECT_LIST=0,
+  OBJECT_STRING,
+  OBJECT_HASHTABLE
+}object_type;
+typedef struct object_t {
+    uint8_t type;
+  char *key;
+  void *value;
+}object;
 #endif
