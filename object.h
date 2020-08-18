8b78db4e457fd8be5de82345679976e2e8a60757
perrynzhou <perrynzhou@gmail.com>
2020-08-18 19:17:29 +0800

    remove corouine

--- object.h
+++ object.h
@@ -8,14 +8,21 @@
 #ifndef _OBJECT_H
 #define _OBJECT_H
 #include <stdint.h>
-typedef enum object_type_t {
-  OBJECT_LIST=0,
+typedef enum object_type_t
+{
+  OBJECT_LIST = 0,
   OBJECT_STRING,
   OBJECT_HASHTABLE
-}object_type;
-typedef struct object_t {
-    uint8_t type;
+} object_type;
+typedef struct object_t
+{
+  uint8_t type;
   char *key;
   void *value;
-}object;
+} object;
+object *object_create(const char *key, void *value, uint8_t type);
+const char *object_key(object *obj);
+uint8_t object_type(object *obj);
+void *object_value(object *obj);
+object *object_destroy(const char *key, void *value, uint8_t type);
 #endif
